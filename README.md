64a5ad69e522d05b45d730a83142a7b1645d86fa
smartlegionlab <smartlegiondev@gmail.com>
2021-12-13 19:01:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# BlockChain
+
+***
+
+
+- `pip install -r requirements.txt`
+
+
+- `python3 blockchain.py`
+
+
+- `curl http://localhost:5000/mine`
+- `curl http://localhost:5000/mine`
+
+
+- `curl http://localhost:5000/chain`
+
+
+- `curl -X POST -H "Content-Type: application/json" -d '{
+    "sender": "d4ee26eee15148ee92c6cd394edd974e",
+    "recipient": "someone-other-address",
+    "amount": 5
+    }' "http://localhost:5000/transactions/new"`
---
+++ blockchain.py
@@ -0,0 +1,284 @@
+import hashlib
+import json
+
+import requests
+
+from time import time
+from urllib.parse import urlparse
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
+
+
+class BlockChain(object):
+    def __init__(self):
+        self.current_transactions = []
+        self.chain = []
+        self.nodes = set()
+        # Создание блока генезиса
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Создание нового блока в блокчейне
+
+        :param proof: <int> Доказательства проведенной работы
+        :param previous_hash: (Опционально) хеш предыдущего блока
+        :return: <dict> Новый блок
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Перезагрузка текущего списка транзакций
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Направляет новую транзакцию в следующий блок
+
+        :param sender: <str> Адрес отправителя
+        :param recipient: <str> Адрес получателя
+        :param amount: <int> Сумма
+        :return: <int> Индекс блока, который будет хранить эту транзакцию
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Создает хэш SHA-256 блока
+
+        :param block: <dict> Блок
+        :return: <str>
+        """
+
+        # Мы должны убедиться в том, что словарь упорядочен, иначе у нас будут непоследовательные хеши
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """
+        Простая проверка алгоритма:
+         - Поиска числа p`, так как hash(pp`) содержит 4 заглавных нуля, где p - предыдущий
+         - p является предыдущим доказательством, а p` - новым
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Подтверждение доказательства: Содержит ли hash(last_proof, proof) 4 заглавных нуля?
+
+        :param last_proof: <int> Предыдущее доказательство
+        :param proof: <int> Текущее доказательство
+        :return: <bool> True, если правильно, False, если нет.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def register_node(self, address):
+        """
+        Вносим новый узел в список узлов
+
+        :param address: <str> адрес узла , другими словами: 'http://192.168.0.5:5000'
+        :return: None
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        """
+        Проверяем, является ли внесенный в блок хеш корректным
+
+        :param chain: <list> blockchain
+        :return: <bool> True если она действительна, False, если нет
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Проверьте правильность хеша блока
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Проверяем, является ли подтверждение работы корректным
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        Это наш алгоритм Консенсуса, он разрешает конфликты,
+        заменяя нашу цепь на самую длинную в цепи
+
+        :return: <bool> True, если бы наша цепь была заменена, False, если нет.
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # Ищем только цепи, длиннее нашей
+        max_length = len(self.chain)
+
+        # Захватываем и проверяем все цепи из всех узлов сети
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Проверяем, является ли длина самой длинной, а цепь - валидной
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Заменяем нашу цепь, если найдем другую валидную и более длинную
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+
+# Создаем экземпляр узла
+app = Flask(__name__)
+
+# Генерируем уникальный на глобальном уровне адрес для этого узла
+node_identifier = str(uuid4()).replace('-', '')
+
+# Создаем экземпляр блокчейна
+block_chain = BlockChain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # Мы запускаем алгоритм подтверждения работы, чтобы получить следующее подтверждение…
+    last_block = block_chain.last_block
+    last_proof = last_block['proof']
+    proof = block_chain.proof_of_work(last_proof)
+
+    # Мы должны получить вознаграждение за найденное подтверждение
+    # Отправитель “0” означает, что узел заработал крипто-монету
+    block_chain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Создаем новый блок, путем внесения его в цепь
+    previous_hash = block_chain.hash(last_block)
+    block = block_chain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Убедитесь в том, что необходимые поля находятся среди POST-данных
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Создание новой транзакции
+    index = block_chain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': block_chain.chain,
+        'length': len(block_chain.chain),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        block_chain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(block_chain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = block_chain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': block_chain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': block_chain.chain
+        }
+
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
